9e63cbf6abde740c5a2885ed0d21d4ef296408a0
Tim Massingham <tim.massingham@ebi.ac.uk>
2010-02-05 15:25:14 +0000

    Update copyright headers

--- src/utility.h
+++ src/utility.h
@@ -2,21 +2,20 @@
  *  Copyright (C) 2010 by Tim Massingham, European Bioinformatics Institute
  *  tim.massingham@ebi.ac.uk
  *
- *  This file is part of the simNGS software for simulating likelihoods
- *  for next-generation sequencing machines.
+ *  This file is part of the AYB base-calling software.
  *
- *  simNGS is free software: you can redistribute it and/or modify
+ *  AYB is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
- *  simNGS is distributed in the hope that it will be useful,
+ *  AYB is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with simNGS.  If not, see <http://www.gnu.org/licenses/>.
+ *  along with AYB.  If not, see <http://www.gnu.org/licenses/>.
  */
  
 #ifndef _UTILITY_H
