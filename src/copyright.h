b0729334efdb6aacae069a983376f19979e2838f
Hazel Marsden <hazelm@ebi.ac.uk>
2010-04-06 15:04:28 +0100

    Generic Licence Text

---
+++ src/copyright.h
@@ -0,0 +1,36 @@
+/* 
+ * Generic licence information
+ *
+ * To use define the following:
+ *  PROGNAME:  name of program
+ *  AUTHOR:    who created
+ *  CONTACT:   contact information, e.g email
+ */
+ 
+#ifndef PROGNAME
+   #error "Expected PROGNAME to be defined"
+#endif
+#ifndef AUTHOR
+   #error "Expected AUTHOR to be defined"
+#endif
+#ifndef CONTACT
+   #error "Expected CONTACT to be defined"
+#endif
+
+"\n"
+"  Copyright (C) 2010 by " AUTHOR ", European Bioinformatics Institute\n"
+"  " CONTACT "\n"
+"\n"
+"  " PROGNAME " is free software: you can redistribute it and/or modify\n"
+"  it under the terms of the GNU General Public License as published by\n"
+"  the Free Software Foundation, either version 3 of the License, or\n"
+"  (at your option) any later version.\n"
+"\n"
+"  " PROGNAME " is distributed in the hope that it will be useful,\n"
+"  but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
+"  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n"
+"  GNU General Public License for more details.\n"
+"\n"
+"  You should have received a copy of the GNU General Public License\n"
+"  along with " PROGNAME ".  If not, see <http://www.gnu.org/licenses/>.\n"
+"\n"
