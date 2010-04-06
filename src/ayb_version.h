ab3b26297bde3026d3a47c3d9ed5746f28149098
Hazel Marsden <hazelm@ebi.ac.uk>
2010-04-06 15:11:35 +0100

    AYB Version Control

---
+++ src/ayb_version.h
@@ -0,0 +1,39 @@
+/*
+ *  File    : ayb_version.h
+ *  Created : 5 Mar 2010
+ *  Author  : Hazel Marsden
+ *  Purpose : Header containing AYB version number
+ *
+ *  Copyright (C) 2010 European Bioinformatics Institute
+ *
+ *  This file is part of the AYB base calling software.
+ *
+ *  AYB is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  AYB is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with AYB.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef AYB_VERSION_H_
+#define AYB_VERSION_H_
+
+/*
+ * Program Version
+ * Before making a release, ensure this file is updated
+ */
+
+const float Version = 1.00;      // First version
+const char* VersionDate = "20100312";
+
+/* keep a history */
+// Version 1.00     20100312    Initial with basic infrastructure
+
+#endif /* AYB_VERSION_H_ */
