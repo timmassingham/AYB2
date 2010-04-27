f2750fb5a3d6a85a457a5432b27823dea1ef34e8
Hazel Marsden <hazelm@ebi.ac.uk>
2010-04-27 18:01:57 +0100

    Testing replaced by AYB init and loop. Control handed to ayb_model for each file.

--- src/ayb_main.c
+++ src/ayb_main.c
@@ -1,6 +1,8 @@
 /**
  * \file ayb_main.c
  * Main AYB module.
+ * Sets up environment before passing control to ayb_model for each intensity file.
+ * Tidy up to finish.
  *//*
  *  Created : 23 Feb 2010
  *  Author  : Hazel Marsden
@@ -27,14 +29,12 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <signal.h>
+#include "ayb_model.h"
 #include "ayb_options.h"
 #include "dirio.h"
 #include "handler.h"
 #include "message.h"
-#include "tile.h"
 #include "xio.h"
-
-#include <unistd.h>         // temp for sleep
 
 
 /* constants */
@@ -49,16 +49,19 @@ static const char *LOG_PREFIX = "ayb_";         ///< Log file prefix, pass to Me
 
 /** Tidy up before exit. Include all module tidyup routines here. */
 static void tidyup() {
-    /* message files file */
+    tidyup_model();
     tidyup_dirio();
     tidyup_message();
 }
 
 /* public functions */
 
-/** Main AYB routine. What does it do? */
+/**
+ * Main AYB routine.
+ * Read program options and set up environment.
+ * Then process each input file before tidy up and exit.
+ */
 int main(int argc, char **argv) {
-    AYBOPT *p_opt;
 
     /* install signal handler   */
     signal(SIGINT, INThandler);
@@ -68,16 +71,6 @@ int main(int argc, char **argv) {
     if (!read_options(argc, argv)) {
         return EXIT_FAILURE;
     }
-    /* get a pointer to the program options */
-    p_opt = myopt();
-
-    /* check number of cycles supplied - hmhm move to module when decide where ncycle to live */
-    message(E_GENERIC_SD, MSG_DEBUG, "ncycle:", p_opt->ncycle);
-    if (p_opt->ncycle <= 0) {
-        message(E_NOCYCLES, MSG_FATAL);;
-        return EXIT_FAILURE;
-    }
-    message(E_CYCLE_SELECT_D, MSG_INFO, p_opt->ncycle);
 
     /* create a message log */
     startup_message(LOG_PREFIX);
@@ -87,69 +80,25 @@ int main(int argc, char **argv) {
         return EXIT_FAILURE;
     }
 
-    /**** test file access ****/
+    if (!startup_model()) {
+        return EXIT_FAILURE;
+    }
 
-    XFILE *fpin = NULL;
-    XFILE *fpout = NULL;
+    /* process each intensity file */
+    XFILE *fp = NULL;
     bool found = true;
-//    int c;
 
     while (found) {
-        fpin = open_next(fpin);
+        fp = open_next(fp);
 
-        if (xisnull_file(fpin)) {
+        if (xisnull_file(fp)) {
             found = false;
         }
         else {
-            /* create an output file from the input */
-            fpout = open_output("out");
-            if (xisnull_file(fpout)) {
-                found = false;
-            }
-            else {
-                /* read and store input */
-                TILE tile;
-                unsigned int nc = p_opt->ncycle;
-                tile = read_known_TILE(fpin, &nc);
-                if (nc < p_opt->ncycle) {
-                    message(E_CYCLESIZE_DD, MSG_WARN, p_opt->ncycle, nc);
-                }
-                if (tile != NULL) {
-                    /* output from store */
-                    show_TILE(fpout, tile, 20);
-                }
-
-//                while ((c = xfgetc(fpin)) != EOF) {
-//                    xfputc(c, fpout);
-//                }
-                free_TILE(tile);
-                xfclose(fpout);
-            }
+            /* analyse this input file */
+            analyse_tile(fp);
         }
     }
-
-
-    /********/
-
-    /**** this part is for testing of log and signal handling ****/
-    /* convert boolean value to string */
-//#define BOOLSTR(b) b ? "true" : "false"
-    //  fprintf(stdout, "A value argument is %d\n", p_opt->aval);
-    //  fprintf(stdout, "A flag argument is %s\n", BOOLSTR(p_opt->aflag));
-
-    /*
-    int anint = 3;
-    float afloat = 123.45;
-
-    for (int i = 0; i < 5; i++) {
-        printf("Sleep for %d seconds; divisor = %d\n", p_opt->aval, anint);
-        sleep(p_opt->aval);
-        afloat /= anint--;
-        printf("float = %0.2f\n", afloat);
-        checksignals();
-    }
-*/
-    /********/
 
     /* tidy up before exit */
     tidyup();
